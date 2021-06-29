7723b43fc1ac05b234b253985ec0f35ac77df216
PrashantAKhunt <prashantkhunt1999@gmail.com>
2021-06-29 14:59:40 +0530

    first commit

---
+++ resources/views/imageUpload.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+ 
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+     
+    <div class="panel panel-primary">
+      <div class="panel-heading"><h2>laravel 8 image upload example</h2></div>
+      <div class="panel-body">
+     
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{{ $message }}</strong>
+        </div>
+        <img src="images/{{ Session::get('image') }}">
+        @endif
+    
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    
+        <form action="{{ route('image.upload.post') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+    
+                <div class="col-md-6">
+                    <input type="file" name="image" class="form-control">
+                </div>
+     
+                <div class="col-md-6">
+                    <button type="submit" class="btn btn-success">Upload</button>
+                </div>
+     
+            </div>
+        </form>
+    
+      </div>
+    </div>
+</div>
+    
+</body>
+</html>
